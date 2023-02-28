4fd60cc16c8cbc784ed3393336a63b46287f3e76
Thani Ath Nain Khurshid <thani@milestonezero.net>
2023-02-28 14:48:57 +0500

    I am stupid and now I must start again. SHOOT ME!!

---
+++ main.py
@@ -0,0 +1,9 @@
+from rekhta.RekhtaDatasetBuilder import RekhtaDatasetBuilder
+
+
+def main():
+    builder = RekhtaDatasetBuilder()
+    builder.build()
+
+if __name__ == '__main__':
+    main()
---
+++ rekhta/RekhtaDatasetBuilder.py
@@ -0,0 +1,6 @@
+class RekhtaDatasetBuilder:
+    def __init__(self) -> None:
+        pass
+    
+    def build(self):
+        print("Thani Ath Nain Khurshid, You are extremly stupid.")
